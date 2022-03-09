e8c6ecd5ca1d0e461f31857729ec89f78d21b882
Alex <cupokoffi@gmail.com>
2022-03-09 18:44:36 -0500

    Feedback style revision

--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -7,11 +7,15 @@ function Features(){
         <div className='features-text'>
             <h1>Quality Designs Made Right</h1>
             <hr className="divider" /> 
-            <p>
-            Suzanne Clemente Handbags aren’t just beautifully styled and well made; the “Causes Series” line is a stylish way to show off one’s passion for a particular cause. The ribbons and colors of the “Causes Series” line represent the following causes: Back the Blue, Breast Cancer Awareness, AIDS Awareness, Autism Awareness, “Support Our Troops” and LGBT Awareness. We are currently developing a line for “Back the Blue”.
-            </p>
             <p className="feedback">
-                We would love to hear feedback from you on your personal cause. We want to inspire positive change through fashion. What’s your cause? “Wear your Cause on your sleeve!”
+            Suzanne Clemente Handbags aren’t just beautifully styled and well made; the “Causes Series” 
+            line is a stylish way to show off one’s passion for a particular cause. The ribbons and colors 
+            of the “Causes Series” line represent the following causes: Back the Blue, Breast Cancer 
+            Awareness, AIDS Awareness, Autism Awareness, “Support Our Troops” and LGBT Awareness. We are 
+            currently developing a line for “Back the Blue”. <br /> 
+            <br /> 
+            We would love to hear feedback from you on your personal cause. We want to inspire positive 
+            change through fashion. What’s your cause? “Wear your Cause on your sleeve!”
                 </p>
             <div className="header-btns">
                 <Link to='services' className='active' smooth={true} duration={1000}><a className='cv-btn1'>Get Started!</a></Link> 
