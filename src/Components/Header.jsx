6f63f2646be9448e3045d5cd83f7b116150dc7eb
Alex <cupokoffi@gmail.com>
2022-03-09 21:07:49 -0500

    AOS

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,18 @@
 import React from "react"; 
 import Navbar from './Navbar'; 
 import { Link } from 'react-scroll'; 
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 
 function Header(){
+    AOS.init();
     return(
         <div id="main"> 
             <Navbar /> 
             <div className='name'>
-                <h1 className='the-header'>Find Unique Styles That Fit Your Every Desire</h1>
-                <p className='details'>Through both great passion and expertise, Suzanne has fabricated a wide variety of products that are sure to draw you in!</p> 
-                <div className='header-btns'>
+                <h1 data-aos="fade-right" data-aos-duration="1000" className='the-header'>Find Unique Styles That Fit Your Every Desire</h1>
+                <p data-aos="fade-right" data-aos-duration="1200" className='details'>Through both great passion and expertise, Suzanne has fabricated a wide variety of products that are sure to draw you in!</p> 
+                <div data-aos="fade-right" data-aos-duration="1800" className='header-btns'>
                     <Link to='features' className='active' smooth={true} duration={1000}><a className='cv-btn'>Learn More</a></Link> 
                 </div>
             </div>
