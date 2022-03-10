e4240a542c984b7aeb84d3b8fd56e999a3a71f24
Alex <cupokoffi@gmail.com>
2022-03-09 20:35:39 -0500

    Logo Bar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from "react"; 
 import { Link } from 'react-scroll'; 
+import logo from "../Images/logo/sc-color-bar-transparent.png"; 
 
 function Navbar(){
     return(
         <>
             <nav>
                 <Link to='main' className='logo' smooth={true} duration={1000} >
-                    <h1>Suzanne Clemente</h1>  
+                    <img src={logo} alt="Suzanne Clemente" />  
                 </Link>
                 <input className='menu-btn' type='checkbox' id='menu-btn'/> 
                 <label className='menu-icon' for='menu-btn'>
