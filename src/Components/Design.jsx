c6079b943ff2213ff3a8f03f92375528ba30f89e
Jadyn <jadyngg19@gmail.com>
2024-01-27 20:53:11 -0500

    Implement new shop link

--- src/Components/Design.jsx
+++ src/Components/Design.jsx
@@ -6,7 +6,7 @@ function Design(){
         <div id="design"> 
             <Slider /> 
             <div className='header-btns'>
-                    <a href='https://www.amazon.com/s?rh=n%3A7141123011%2Cp_4%3ASuzanne+Clemente&ref=bl_sl_s_ap_web_7141123011' className='cv-btn'>Shop Now</a> 
+                    <a href='https://www.suzanne-clemente.store' className='cv-btn'>Shop Now</a> 
                 </div>
                 <br /> 
         </div> 
